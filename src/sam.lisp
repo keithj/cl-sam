d6665503d0f395cd27a5447b7fedb7b6d59f5f3b
Keith James <dev@deoxybyte.co.uk>
2009-08-01 12:31:13 +0100

    * Added SAM header parsing functions.

--- src/sam.lisp
+++ src/sam.lisp
@@ -19,5 +19,105 @@
 
 (in-package :sam)
 
-;;; This space intentionally blank. SAMtools has provided all my
-;;; SAM-reading needs so far.
+(defparameter *mandatory-tags* (pairlis '(:hd :sq :rg :pg)
+                                        '((:vn) (:sn :ln) (:id :sm) (:id)))
+  "A mapping that describes the mandatory tags for each SAM header
+  record type.")
+
+(defmacro define-tag-parser (name (record-type var) tag-specs)
+  "Defines a tag parsing function NAME that parses tag values for sam
+header RECORD-TYPE."
+  `(progn
+    (defun ,name (str)
+      (unless (and (< 3 (length str))
+                   (char= #\: (char str 2)))
+        (error 'malformed-field-error :field str
+               :text (format nil "missing colon in tag:value")))
+      (let ((tag (subseq str 0 2))
+            (,var (subseq str 3)))
+        (cond ,@(loop
+                   for (tag-val sym form) in tag-specs
+                   collect `((string= ,tag-val tag)
+                             (list ,sym ,(if form
+                                            `,form
+                                          `,var))))
+              (t
+               (error 'malformed-field-error :field str
+                      :text (format nil "invalid ~a tag ~a"
+                                    ,record-type tag))))))))
+
+(define-tag-parser parse-hd-tag ("HD" value)
+  (("VN" :vn)
+   ("SO" :so (cond ((string= "unsorted" value)
+                    :unsorted)
+                   ((string= "queryname" value)
+                    :queryname)
+                   ((string= "coordinate" value)
+                    :coordinate)
+                   (t
+                    (error 'malformed-field-error :field value
+                           :text "invalid SO value"))))
+   ("GO" :go (cond ((string= "none" value)
+                    :none)
+                   ((string= "query" value)
+                    :query)
+                   ((string= "reference" value)
+                    :reference)
+                   (t
+                    (error 'malformed-field-error :field value
+                           :text "invalid GO value"))))))
+
+(define-tag-parser parse-sq-tag ("SQ" value)
+  (("SN" :sn) ("LN" :ln (parse-integer value))
+   ("AS" :as) ("M5" :m5) ("UR" :ur) ("SP" :sp)))
+
+(define-tag-parser parse-rg-tag ("RG" value)
+  (("ID" :id) ("SM" :sm) ("LB" :lb) ("DS" :ds) ("PU" :pu) ("PI" :pi)
+   ("CN" :cn) ("DT" :dt) ("PL" :pl)))
+
+(define-tag-parser parse-pg-tag ("PG" value)
+  (("ID" :id) ("VN" :vn) ("CL" :cl)))
+
+(defun mandatory-tags (record-type)
+  "Returns a list of the mandatory tags for SAM header RECORD-TYPE."
+  (rest (assoc record-type *mandatory-tags*)))
+
+(defun ensure-mandatory-tags (header-record)
+  "Returns HEADER-RECORD if all mandatory tags are present, or raises
+a {define-condition malformed-record-error} ."
+  (let ((tag-type (first header-record))
+        (tags (rest header-record)))
+    (if (subsetp (mandatory-tags tag-type) (mapcar #'first tags))
+        header-record
+      (error 'malformed-record-error
+             :record header-record
+             :text (format nil "missing mandatory ~a tags"
+                           (set-difference 
+                            (mandatory-tags tag-type)
+                            (mapcar #'first tags)))))))
+
+(defun parse-header-record (str)
+  "Parses a single SAM header record STR and returns an alist. May
+raise a {define-condition malformed-record-error} or
+{define-condition malformed-field-error} ."
+  (flet ((tags (fn)
+           (mapcar fn (rest (string-split str #\Tab)))))
+    (cond ((starts-with-string-p str "@HD")
+           (cons :hd (tags #'parse-hd-tag)))
+          ((starts-with-string-p str "@SQ")
+           (cons :sq (tags #'parse-sq-tag)))
+          ((starts-with-string-p str "@RG")
+           (cons :rg (tags #'parse-rg-tag)))
+          ((starts-with-string-p str "@PG")
+           (cons :pg (tags #'parse-pg-tag)))
+          (t
+           (error 'malformed-record-error
+                  :record str
+                  :text "invalid SAM header record")))))
+
+(defun sam-header (str)
+  (with-input-from-string (s str)
+    (loop
+       for rec = (read-line s nil nil)
+       while rec
+       collect (parse-header-record rec))))
